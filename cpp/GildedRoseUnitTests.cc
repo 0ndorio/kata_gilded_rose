1ec7b1bad2be87f49969b6d8b26184d77c4ca4a2
Bruno Kirschner <bruno.kirschner@online.de>
2016-04-15 22:01:25 +0200

    [CPP] Adds test for quality decrease after sell in date passed.

--- cpp/GildedRoseUnitTests.cc
+++ cpp/GildedRoseUnitTests.cc
@@ -1,2 +1,19 @@
 #include <gtest/gtest.h>
 #include "GildedRose.h"
+
+TEST(GildedRoseUnitTest, QualityDecreasesTwiceAsFastAfterSellDatePassed)
+{
+  int quality = 10;
+  int sell_in = 1;
+
+  std::vector<Item> items{{"Generic Item", sell_in, quality}};
+  GildedRose app{items};
+
+  app.updateQuality();
+  EXPECT_EQ(sell_in - 1, items[0].sellIn);
+  EXPECT_EQ(quality - 1, items[0].quality);
+
+  app.updateQuality();
+  EXPECT_EQ(sell_in - 2, items[0].sellIn);
+  EXPECT_EQ(quality - 3, items[0].quality);
+}
