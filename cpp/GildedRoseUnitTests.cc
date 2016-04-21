53356695959252ba99022449442774ad754457c3
Bruno Kirschner <bruno.kirschner@online.de>
2016-04-21 15:36:44 +0200

    [CPP|Tests] Adds backstage pass related tests.

--- cpp/GildedRoseUnitTests.cc
+++ cpp/GildedRoseUnitTests.cc
@@ -95,3 +95,72 @@ TEST(SulfurasTest, QualityAndSellInNeverAlters)
   EXPECT_EQ(sell_in, items[0].sellIn);
   EXPECT_EQ(quality, items[0].quality);
 }
+
+//
+// Backstage Pass Tests
+//
+
+TEST(BackstagePassTest, QualityIncreasesBy1WhenSellInIsAbove10)
+{
+  auto quality = 10;
+  auto sell_in = 12;
+
+  std::vector<Item> items{{"Backstage passes to a TAFKAL80ETC concert", sell_in, quality}};
+  GildedRose app{items};
+
+  app.updateQuality();
+  EXPECT_LT(10, items[0].sellIn);
+  EXPECT_EQ(quality + 1, items[0].quality);
+}
+
+TEST(BackstagePassTest, QualityIncreasesBy2WhenSellInDropsBelow10)
+{
+  auto quality = 10;
+  auto sell_in = 11;
+
+  std::vector<Item> items{{"Backstage passes to a TAFKAL80ETC concert", sell_in, quality}};
+  GildedRose app{items};
+
+  app.updateQuality();
+  EXPECT_EQ(10, items[0].sellIn);
+  EXPECT_EQ(quality + 1, items[0].quality);
+
+  app.updateQuality();
+  EXPECT_GT(10, items[0].sellIn);
+  EXPECT_EQ(quality + 3, items[0].quality);
+}
+
+TEST(BackstagePassTest, QualityIncreasesBy3WhenSellInDropsBelow5)
+{
+  auto quality = 10;
+  auto sell_in = 6;
+
+  std::vector<Item> items{{"Backstage passes to a TAFKAL80ETC concert", sell_in, quality}};
+  GildedRose app{items};
+
+  app.updateQuality();
+  EXPECT_EQ(5, items[0].sellIn);
+  EXPECT_EQ(quality + 2, items[0].quality);
+
+  app.updateQuality();
+  EXPECT_GT(5, items[0].sellIn);
+  EXPECT_EQ(quality + 5, items[0].quality);
+}
+
+TEST(BackstagePassTest, QualityIsZeroAfterTheConcert)
+{
+  auto quality = 10;
+  auto sell_in = 1;
+
+  std::vector<Item> items{{"Backstage passes to a TAFKAL80ETC concert", sell_in, quality}};
+  GildedRose app{items};
+
+  app.updateQuality();
+  EXPECT_EQ(0, items[0].sellIn);
+  EXPECT_LT(quality, items[0].quality);
+
+  app.updateQuality();
+  EXPECT_GT(0, items[0].sellIn);
+  EXPECT_EQ(0, items[0].quality);
+}
+
